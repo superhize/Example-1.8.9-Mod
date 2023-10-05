72715efafe3d78a6cde64036b8c245e0e7c128a4
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2023-10-05 21:59:56 +0200

    did something until the config gui shows textures again

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     idea
     java
     id("gg.essential.loom") version "0.10.0.+"
     id("dev.architectury.architectury-pack200") version "0.1.3"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    kotlin("jvm") version "1.8.21"
+    kotlin("jvm") version "1.9.0"
+    id("com.bnorm.power.kotlin-power-assert") version "0.13.0"
 }
 
 //Constants:
@@ -18,28 +21,6 @@ val modid: String by project
 // Toolchains:
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(8))
-}
-
-// Minecraft configuration:
-loom {
-    log4jConfigs.from(file("log4j2.xml"))
-    launchConfigs {
-        "client" {
-            // If you don't want mixins, remove these lines
-            property("mixin.debug", "true")
-            property("asmhelper.verbose", "true")
-            arg("--tweakClass", "org.spongepowered.asm.launch.MixinTweaker")
-        }
-    }
-    forge {
-        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
-        // If you don't want mixins, remove this lines
-        mixinConfig("mixins.$modid.json")
-    }
-    // If you don't want mixins, remove these lines
-    mixin {
-        defaultRefmapName.set("mixins.$modid.refmap.json")
-    }
 }
 
 tasks.compileJava {
@@ -105,10 +86,58 @@ dependencies {
 
     shadowModImpl(libs.moulconfig)
     devenvMod(variantOf(libs.moulconfig) { classifier("test") })
-//
+
     shadowImpl(libs.libautoupdate)
     shadowImpl("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
 
+//    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+tasks.withType(Test::class) {
+    useJUnitPlatform()
+    javaLauncher.set(javaToolchains.launcherFor(java.toolchain))
+    workingDir(file("run"))
+    systemProperty("junit.jupiter.extensions.autodetection.enabled", "true")
+}
+
+kotlin {
+    sourceSets.all {
+        languageSettings {
+            languageVersion = "2.0"
+            enableLanguageFeature("BreakContinueInInlineLambdas")
+        }
+    }
+}
+
+// Minecraft configuration:
+loom {
+    launchConfigs {
+        "client" {
+            // If you don't want mixins, remove these lines
+            property("mixin.debug", "true")
+            property("asmhelper.verbose", "true")
+            arg("--tweakClass", "org.spongepowered.asm.launch.MixinTweaker")
+            arg("--mixin", "mixins.$modid.json")
+            val modFiles = devenvMod
+                .incoming.artifacts.resolvedArtifacts.get()
+            arg("--mods", modFiles.joinToString(",") { it.file.relativeTo(file("run")).path })
+        }
+    }
+    forge {
+        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
+        // If you don't want mixins, remove this lines
+        mixinConfig("mixins.$modid.json")
+    }
+    // If you don't want mixins, remove these lines
+    mixin {
+        defaultRefmapName.set("mixins.$modid.refmap.json")
+    }
+    runConfigs {
+        "server" {
+            isIdeConfigGenerated = false
+        }
+    }
 }
 
 // Tasks:
@@ -119,6 +148,7 @@ tasks.withType(JavaCompile::class) {
 
 tasks.withType(Jar::class) {
     archiveBaseName.set(modid)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     manifest.attributes.run {
         this["FMLCorePluginContainsFMLMod"] = "true"
         this["ForceLoadAsMod"] = "true"
@@ -157,16 +187,28 @@ tasks.jar {
 tasks.shadowJar {
     destinationDirectory.set(layout.buildDirectory.dir("badjars"))
     archiveClassifier.set("all-dev")
-    configurations = listOf(shadowImpl)
+    configurations = listOf(shadowImpl, shadowModImpl)
     doLast {
         configurations.forEach {
             println("Copying jars into mod: ${it.files}")
         }
     }
+    exclude("META-INF/versions/**")
 
     // If you want to include other dependencies and shadow them, you can relocate them in here
     fun relocate(name: String) = relocate(name, "$baseGroup.deps.$name")
 }
-
+tasks.jar {
+    archiveClassifier.set("nodeps")
+    destinationDirectory.set(layout.buildDirectory.dir("badjars"))
+}
 tasks.assemble.get().dependsOn(tasks.remapJar)
 
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
