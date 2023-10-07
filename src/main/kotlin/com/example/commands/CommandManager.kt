e690f2558ad6e8adbca9615a8cdb631d13ca8986
CalMWolfs <94038482+CalMWolfs@users.noreply.github.com>
2023-10-07 20:21:37 +1100

    Merge pull request #3
    * no java

--- src/main/kotlin/com/example/commands/CommandManager.kt
+++ src/main/kotlin/com/example/commands/CommandManager.kt
@@ -4,6 +4,7 @@ import com.example.ExampleMod
 import com.example.commands.SimpleCommand.ProcessCommandRunnable
 import com.example.utils.ChatUtils
 import net.minecraft.command.ICommandSender
+import net.minecraft.util.BlockPos
 import net.minecraftforge.client.ClientCommandHandler
 
 class CommandManager {
@@ -17,28 +18,30 @@ class CommandManager {
         }
     }
 
-    private fun registerCommand(
-        name: String,
-        function: (Array<String>) -> Unit
-    ) = registerCommand0(name, function)
+    private fun registerCommand(name: String, function: (Array<String>) -> Unit) {
+        ClientCommandHandler.instance.registerCommand(SimpleCommand(name, createCommand(function)))
+    }
 
     private fun registerCommand0(
         name: String,
         function: (Array<String>) -> Unit,
         autoComplete: ((Array<String>) -> List<String>) = { listOf() }
     ) {
-        ClientCommandHandler.instance.registerCommand(
-            SimpleCommand(
-                name,
-                createCommand(function)
-            ) { _, b, _ -> autoComplete(b) }
+        val command = SimpleCommand(
+            name,
+            createCommand(function),
+            object : SimpleCommand.TabCompleteRunnable {
+                override fun tabComplete(sender: ICommandSender?, args: Array<String>?, pos: BlockPos?): List<String> {
+                    return autoComplete(args ?: emptyArray())
+                }
+            }
         )
+        ClientCommandHandler.instance.registerCommand(command)
     }
 
-    private fun createCommand(function: (Array<String>) -> Unit) =
-        object : ProcessCommandRunnable() {
-            override fun processCommand(sender: ICommandSender?, args: Array<out String>) {
-                function(args.asList().toTypedArray())
-            }
+    private fun createCommand(function: (Array<String>) -> Unit) = object : ProcessCommandRunnable() {
+        override fun processCommand(sender: ICommandSender?, args: Array<String>?) {
+            if (args != null) function(args.asList().toTypedArray())
         }
+    }
 }
